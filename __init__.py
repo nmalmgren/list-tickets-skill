f22380fa1e9d7d8c64c6a5355d4ad07eeda15b0a
Natalie Malmgren <nmalmgren@sandiego.edu>
2021-03-16 15:09:50 -0700

    added intent handler for listing tickets

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+import sqlite3
+from sqlite3 import Error
 
 
 class ListTickets(MycroftSkill):
@@ -7,7 +9,22 @@ class ListTickets(MycroftSkill):
 
     @intent_file_handler('tickets.list.intent')
     def handle_tickets_list(self, message):
-        self.speak_dialog('tickets.list')
+        database1 = "cubic.sql"
+
+        # create a database connection
+        conn = sqlite3.connect(database1)
+        cur = conn.cursor()
+    
+        cur.execute("SELECT * FROM PassData")
+        rows = cur.fetchall()
+        i=1
+        self.speak_dialog('tickets.list.dialog')
+        for row in rows:
+            cur.execute("SELECT * FROM TransitLine WHERE LineID = ?", (row[3],))
+            idrow = cur.fetchone()
+            self.speak('Pass {} starts at {} ends at {} has an E T A of {} and costs {}'.format(i, row[4], row[5], idrow[3], row[6]))
+            i += 1
+        conn.close()
 
 
 def create_skill():
